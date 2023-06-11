ee6b416e6f6afc8ffe8c9cfe7a39598d3218bcd1
swookjy_ubuntu <swookjy@gmail.com>
2023-06-11 21:53:19 +0900

    0613 5

--- 0613/echo_EPLTserv.c
+++ 0613/echo_EPLTserv.c
@@ -25,33 +25,33 @@ int main(int argc, char *argv[])
 	struct epoll_event event;
 	int epfd, event_cnt;
 
-	if(argc!=2) {
+	if(argc != 2) {
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
 
-	serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 	memset(&serv_adr, 0, sizeof(serv_adr));
-	serv_adr.sin_family=AF_INET;
-	serv_adr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_adr.sin_port=htons(atoi(argv[1]));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_adr.sin_port = htons(atoi(argv[1]));
 	
-	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr))==-1)
+	if(bind(serv_sock, (struct sockaddr*) &serv_adr, sizeof(serv_adr)) == -1)
 		error_handling("bind() error");
-	if(listen(serv_sock, 5)==-1)
+	if(listen(serv_sock, 5) == -1)
 		error_handling("listen() error");
 
-	epfd=epoll_create(EPOLL_SIZE);
-	ep_events=malloc(sizeof(struct epoll_event)*EPOLL_SIZE);
+	epfd = epoll_create(EPOLL_SIZE);
+	ep_events = malloc(sizeof(struct epoll_event)*EPOLL_SIZE);
 
-	event.events=EPOLLIN;
-	event.data.fd=serv_sock;	
+	event.events = EPOLLIN;
+	event.data.fd = serv_sock;	
 	epoll_ctl(epfd, EPOLL_CTL_ADD, serv_sock, &event);
 
 	while(1)
 	{
-		event_cnt=epoll_wait(epfd, ep_events, EPOLL_SIZE, -1);
-		if(event_cnt==-1)
+		event_cnt = epoll_wait(epfd, ep_events, EPOLL_SIZE, -1);
+		if(event_cnt == -1)
 		{
 			puts("epoll_wait() error");
 			break;
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
         for(i=0; i<event_cnt; i++){
             if(ep_events[i].data.fd == serv_sock){
                 adr_sz = sizeof(clnt_adr);
-                clnt_sock = accpet(serv_sock, (struct sockaddr*)&clnt_adr, &adr_sz);
+                clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &adr_sz);
 
                 event.events = EPOLLIN;
                 event.data.fd = clnt_sock;
@@ -70,12 +70,13 @@ int main(int argc, char *argv[])
             }
             else{
                 str_len = read(ep_events[i].data.fd, buf, BUF_SIZE);
-                if(str_len == 0){ // close request
+                if(str_len == 0){ 		// close request!
                     epoll_ctl(epfd, EPOLL_CTL_DEL, ep_events[i].data.fd, NULL);
-                    close(ep_events[i].data.fd);//socket file descriptor
+                    close(ep_events[i].data.fd);
                     printf("closed client : %d\n", ep_events[i].data.fd);
                 }
-                else{       // echo service
+                else{       			// echo !
+					//write(1, buf, strlen(buf));
                     write(ep_events[i].data.fd, buf, str_len);
                 }
             }
